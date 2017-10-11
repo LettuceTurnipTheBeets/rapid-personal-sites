5c6f43c48710e3d7c54d52bcd00abc8b597eb09d
Marty Corbett <corbe054@umn.edu>
2017-10-11 18:51:03 +0000

    Still get TemplateDoesNotExist error

--- personal/urls.py
+++ personal/urls.py
@@ -6,15 +6,11 @@ from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
-from search import views as search_views
-
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
-    url(r'^search/$', search_views.search, name='search'),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
